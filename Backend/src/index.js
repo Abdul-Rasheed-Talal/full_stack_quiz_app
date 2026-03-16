74fb615e85863332af4ba776edea8418d76d2c97
M. Abdul-Rasheed Talal <mabdulrasheedtalal@gmail.com>
2026-03-16 16:06:35 +0500

    connect database using professional approach

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -1,15 +1,17 @@
-import express from 'express'
-import 'dotenv/config';
+import express from "express";
+import "dotenv/config";
+import mongoose from "mongoose";
+import { DB_NAME } from "./constants.js";
+import connectDB from "./db/db.js";
 
-const app = express()
-const port = process.env.port
-console.log(port);
+connectDB();
+const app = express();
+const port = process.env.port;
 
+// app.get('/' , (req , res)=>{
+//     res.send('server running')
+// });
 
-app.get('/' , (req , res)=>{
-    res.send('server running')
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`);
 });
-
-app.listen(port , ()=>{
-    console.log(`Listening on port ${port}`);
-})
